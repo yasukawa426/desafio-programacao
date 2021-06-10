8bf06063b8983421f7d20795eb01f5ba3cc70ae1
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2021-06-10 00:04:47 -0300

    Magic Eightball feito

---
+++ src/javascript/eightball.js
@@ -0,0 +1,24 @@
+let respostas = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes, definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
+indiceAleatorio = ((respostas.length-1) * parseFloat(Math.random().toFixed(1))).toFixed(0)
+console.log("The magic ball answered: ", respostas[indiceAleatorio]);
