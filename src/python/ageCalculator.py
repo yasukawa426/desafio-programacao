5eb7bf79401c3c37254dfb7f49cf6dd9cc61040f
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2021-06-17 03:11:58 -0300

    Está perguntando a idade e sabe diferenciar o ano de nascimento, da idade em si

---
+++ src/python/ageCalculator.py
@@ -0,0 +1,12 @@
+# esse programa transforma a sua idade em segundos
+
+idade = int(input("Digite sua idade em anos ou digite o seu ano de nascimento: "))
+
+# significa q o usuario digitou o ano de nascimento
+if idade > 1000:
+    # fazer algo
+    print("não salve")
+
+# digitou a idade
+else:
+    print("salve")
