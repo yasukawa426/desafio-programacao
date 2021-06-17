792bac38aa6e46ec1bbbb820ca5630174cd30bd9
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2021-06-17 03:20:43 -0300

    Ta calculando agr baseado no ano de nascimento

--- src/python/ageCalculator.py
+++ src/python/ageCalculator.py
@@ -1,11 +1,19 @@
+from datetime import date
 # esse programa transforma a sua idade em segundos
 
 idade = int(input("Digite sua idade em anos ou digite o seu ano de nascimento: "))
 
 # significa q o usuario digitou o ano de nascimento
 if idade > 1000:
-    # fazer algo
-    print("não salve")
+    hoje = date.today()
+    hoje = hoje.year
+    # print("Today's date:", hoje)
+    # pega quantos anos a pessoa tem
+    idade = hoje - idade
+    # transforam e msegundos
+    idade = idade * 31556926
+    print(f"Sua idade em segundos é {idade}")
+
 
 # digitou a idade
 else:
@@ -14,3 +22,4 @@ else:
     # transforma em segundos
     idade = idade * 31556926
     print(f"Sua idade em segundos é: {idade}")
+
